1d838c8521ccd8f456f229db5e12ec85cf1701fc
Naren Doraiswamy <narendoraiswamy@gmail.com>
2022-04-15 00:52:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import React from 'react';
 // atob is deprecated but this function converts base64string to text string
 const decodeFileBase64 = (base64String) => {
   // From Bytestream to Percent-encoding to Original string
-  return decodeURIComponent(
-    atob(base64String).split("").map(function (c) {
-      return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join("")
-  );
+  return "data:image/png;base64," + base64String
+  //return decodeURIComponent(
+  //  atob(base64String).split("").map(function (c) {
+  //    return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+  //  }).join("")
+  //);
 };
 
 
@@ -106,7 +107,8 @@ function App() {
       </div>
       <div className="Output">
         <h1>Results</h1>
-        <p>{outputFileData}</p>
+        <img src={outputFileData} alt="" />
+        //<p>{outputFileData}</p>
       </div>
     </div>
   );
